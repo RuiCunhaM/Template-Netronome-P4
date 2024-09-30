b77a1b1312b91948ee8c35ff1c2880824923cd7a
lumafepe <lumafepe@gmail.com>
2024-09-30 14:47:24 +0100

    fix #pragma netro no_lookup_caching not being able to have spaces and tabs before the instructions

--- utils/fix-cache-bypass.sh
+++ utils/fix-cache-bypass.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
-grep "#pragma netro no_lookup_caching" src/*.p4 | awk '{print $5}' | python3 utils/create-updated-actions.py
+grep "#pragma netro no_lookup_caching" src/*.p4 | sed 's/^[ \t]*//' | awk '{print $4}' | python3 utils/create-updated-actions.py
 mv pifs/updated_pif_actions.c pifs/pif_actions.c
